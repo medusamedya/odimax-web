8753774c9460120acece72f6942086b0b43b064f
Berkay Kaptan Sara <kaptanberkay35@hotmail.com>
2026-07-24 17:43:32 +0300

    Contact linki düzeltildi

--- src/app/components/pricing/PricingCards.tsx
+++ src/app/components/pricing/PricingCards.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Image from "next/image";
-import { Link } from "lucide-react";
+import Link from "next/link";
 
 const FEATURES = [
   "Hasta & randevu yönetimi",
