5fd8c93e94ccfb3c9f5ce1b16991c7fd6c40fe83
Berkay Kaptan Sara <kaptanberkay35@hotmail.com>
2026-08-13 11:04:41 +0300

    Testimonials basharflerr düzeltildi

--- src/data/testimonialsData.ts
+++ src/data/testimonialsData.ts
@@ -16,7 +16,7 @@ export const testimonialsData = [
     role: "",
     content: "5'ten fazla merkezemize  Odimaxı aldığımızdan beri iş akışlarımız , hasta takip ve randevu yönetimlerimiz çok hızlandı.Biz ana merkezden tüm işleyişi görebiliyoruz.Bir sorun olduğunda da hemen ilgileniyorlar , başta Gizem Hanım olmak üzere ekibe teşekkürler.",
     rating: 5,
-    avatar: "https://ui-avatars.com/api/?name=Ayse+Yilmaz&background=0b1120&color=fff",
+    avatar: "https://ui-avatars.com/api/?name=Faruk+Özdemir&background=0b1120&color=fff",
     googleLink: "https://www.google.com/search?sa=X&sca_esv=2c719d755bc25006&sxsrf=APpeQntUS7tUZ5uNocaQc-wdjf7Hvtm1VQ:1785414137777&q=Odimax+Yorumlar&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDeyNDI2szQzMbKwNDQ2NrA0Mt_AyPiKkd8_JTM3sUIhMr-oNDcnsWgRK7oIAMSl5Ho_AAAA&rldimm=17292369642891330927&tbm=lcl&hl=tr-TR&ved=2ahUKEwj_6dX7sfqVAxVKA9sEHQkfA_gQ9fQKegQIQhAG&biw=1920&bih=945&dpr=1#lkt=LocalPoiReviews"
   },
   {
@@ -25,7 +25,7 @@ export const testimonialsData = [
     role: "",
     content: "Finans, stok ve hasta takibini ayrı ayrı programlarda yapmaktan kurtulduk. Odimax sayesinde her şey tek bir yerde, bulut tabanlı ve çok güvenli. Kesinlikle tüm meslektaşlarıma tavsiye ederim.",
     rating: 5,
-    avatar: "https://ui-avatars.com/api/?name=Kemal+Demir&background=02b6da&color=fff",
+    avatar: "https://ui-avatars.com/api/?name=Tanju+Çelebi&background=02b6da&color=fff",
     googleLink: "https://www.google.com/search?sa=X&sca_esv=2c719d755bc25006&sxsrf=APpeQntUS7tUZ5uNocaQc-wdjf7Hvtm1VQ:1785414137777&q=Odimax+Yorumlar&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDeyNDI2szQzMbKwNDQ2NrA0Mt_AyPiKkd8_JTM3sUIhMr-oNDcnsWgRK7oIAMSl5Ho_AAAA&rldimm=17292369642891330927&tbm=lcl&hl=tr-TR&ved=2ahUKEwj_6dX7sfqVAxVKA9sEHQkfA_gQ9fQKegQIQhAG&biw=1920&bih=945&dpr=1#lkt=LocalPoiReviews"
   },
 ];
